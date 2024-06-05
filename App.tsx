92b4f4972ebf64a1fb8aea206981f06ac53b9d8d
abhilash-1411 <Abhilash.Chandravanshi@adixoo.com>
2024-06-05 15:14:08 +0530

    inline,internal and external styles

--- App.tsx
+++ App.tsx
@@ -1,118 +1,39 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
 
 import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
+import Exstyles from './style'
+
+import {StyleSheet, Text, View,
 } from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+const App=()=>{
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View>
+    
+      <Text style={{fontSize:30,backgroundColor:'green',margin:4}}>Style in React-Native</Text>
+      <Text style={styles.textBox}>Style in React-Native</Text>
+      <Text style={styles.textBox}>Style in React-Native</Text>
+      <Text style={Exstyles.textBox}> External Style in React-Native</Text>
+      <Text style={[styles.textBox,Exstyles.textBox,{marginTop:10,height:100}]}> External,Internal,Inline Style in React-Native</Text>
     </View>
-  );
+      );
+   
+       
 }
+const styles=StyleSheet.create({
+  textBox:{
+    color:'red',
+    backgroundColor:'blue',
+    fontSize:30,
+    marginBottom:10,
+    padding:5,
+    borderRadius:10,
+    height:60,
+    textAlignVertical:'center',
+    textAlign:'center',
+    borderWidth:2,
+    borderColor:'red'
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
+  }
+})
+ 
 export default App;
---
+++ style.js
@@ -0,0 +1,11 @@
+import { StyleSheet } from "react-native"
+export default Exstyles=StyleSheet.create({
+    textBox:{
+      color:'red',
+      backgroundColor:'yellow',
+      fontSize:30
+      
+  
+  
+    }
+})
